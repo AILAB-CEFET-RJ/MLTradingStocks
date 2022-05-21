3ed7ba4f2005ec0e7ccef74a9cb1e93d7683833b
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-05-21 19:32:31 -0300

    Modification for main.py (to run tests for all csv files) and exclusion of unused files.

--- README.md
+++
@@ -1,6 +0,0 @@
-# MLTradingStocks
-
-O algoritmo de Reinforcement Learning aplicado a trading utiliza como base as ações da Apple. De forma que o algoritmo de Machine Learning funcione, a biblioteca stable-baselines3 precisa ser instalada e, para que esta funcione, a biblioteca torch também teve que ser isntalada (versão 1.4.0). Além disso, as bibliotecas gym (para criação de ambiente customizado para o algoritmo de RL), pandas, numpy e random (para análises e tratamentos de dados) também foram utilizadas.
-
-A classe StockTradingEnvironment tem por objetivo criar o ambiente sobre o qual o modelo será treinado para, a partir do treino, adotar as melhores estratégias de ivnestimento para o ativo analisado.
-
--- activities/rl_boleta/main.py
+++ activities/rl_boleta/main.py
@@ -1,33 +1,34 @@
-from cgi import test
-import rl_model
 from training import train_agent
 from testing import test_agent
 import csv
 from datetime import datetime
 
-header = ['base de treinamento', 'treinamento - condição de parada', 'quantidade de episódios terminais treino', \
+header = ['run', 'base de treinamento', 'treinamento - condição de parada', 'quantidade de episódios terminais treino', \
 'recompensas treino', 'valor inicial treino', 'valor final treino', 'lucro/prejuízo treino', 'base de teste', \
 'quantidade de episódios teste', 'recompensas teste', 'valor inicial teste', 'valor final teste', 'lucro/prejuízo teste', \
 'quantidade de passos teste']
+files_training = ['consolidado_treinamento (01.12 a 06.12).csv']
+files_testing = ['consolidado_teste (10.01 a 14.01).csv', 'consolidado_teste (18.01 a 26.01).csv', 'consolidado_teste (21.02 a 11.03).csv', 'consolidado_teste (23.03 a 28.03).csv']
 
 string_now = datetime.now().strftime('%d_%m_%Y_%H:%M:%S')
 
 
-for i in range(1):
-    results_training = train_agent('consolidado_treinamento (01.12 a 06.12).csv', i)
+for i in range(50):
+    for file_training in files_training:
+        results_training = train_agent(file_training, i)
 
-    results_testing = test_agent('consolidado_teste (10.01 a 14.01).csv', i)
-#     test_agent('consolidado_teste (18.01 a 26.01).csv')
-#     test_agent('consolidado_teste (21.02 a 11.03).csv')
-#     test_agent('consolidado_teste (23.03 a 28.03).csv')
-    
-    results = {**results_training, **results_testing}
-    if i == 0:
-        mode = 'w'
-    else:
-        mode = 'a'
+        for file_testing in files_testing:
+            results_testing = test_agent(file_testing, i)
+            
+            run = {'run': i + 1}
+            results = {**run, **results_training, **results_testing}
+            if i == 0 and file_testing == files_testing[0]:
+                mode = 'w'
+            else:
+                mode = 'a'
 
-    with open(f'{string_now}.csv', mode, encoding='UTF8', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=header)
-        writer.writeheader()
-        writer.writerow(results)
+            with open(f'{string_now}.csv', mode, encoding='UTF8', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=header)
+                if i == 0 and file_testing == files_testing[0]:
+                    writer.writeheader()
+                writer.writerow(results)
